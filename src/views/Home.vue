96ba3427f423144a10888c56401dfe114f476e53
luisVidal97 <luisfcv97@gmail.com>
2020-07-29 09:59:46 -0500

    my first proyect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row>
+      <h1>Categorias</h1>
+    </v-row>
+    <v-row>
+      <v-col v-for="(item,index) in categorias" :key="index">
+
+        <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src= "item.urlImage"
+    >
+    </v-img>
+    <v-card-title> {{item.nombre}}</v-card-title>
+
+    <v-card-text class="text--primary">
+      <div>{{item.descripcion}}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+        to='/products'
+      >
+        Ver productos
+      </v-btn>
+
+    </v-card-actions>
+  </v-card>
+
+
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      categorias:[]
+    }
+  },
+  created(){
+    this.getCategorias();
+  },
+  methods:{
+    getCategorias(){
+      this.axios.get('http://localhost:1337/categorias').then((response) => {
+        console.log(response.data);
+        this.categorias = response.data;
+      })
+    }
   }
 }
 </script>
---
+++ src/views/Products.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-container>
+        <v-row>
+            <h1>Estos son los celulares</h1>
+        </v-row>
+        <v-row>
+            <v-col
+        v-for="n in 3"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+         <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cnet3.cbsistatic.com/img/k8D-4A5ls8OzDmPyImx7VF_0AUU=/2020/04/07/5a7fcdbd-b99a-4f9b-9a33-25fc1db4dfd5/galaxy-s20.jpg"
+    >
+    </v-img>
+    <v-card-title>Samsung S20</v-card-title>
+
+    <v-card-subtitle class="pb-0">$4.000.000</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>El mejor celular gama alta de Samsung</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+      </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
